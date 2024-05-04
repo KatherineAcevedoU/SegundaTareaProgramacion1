010b9d5eda98236a6d035ef51cc01f662b954ef8
Katherine Acevedo <katherineacecedo543@gmail.com>
2024-05-04 12:10:28 -0600

    Se creo el proyecto EjercicioDeEmpleado

---
+++ EjercicioDeEmpleado/EjercicioDeEmpleado/Empleado.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeEmpleado
+{
+    internal class Empleado
+    {
+
+        //Variables de un empleado
+
+        private string nombreEmpleado;
+        private int numeroEmpleado;
+        private decimal salarioEmpleado;
+        private bool estadoEmpleado;
+
+        //Constructor con parametro
+        public Empleado(string nombre, int numero, decimal salario, bool estado = true)
+        {
+            this.nombreEmpleado = nombre;
+            this.numeroEmpleado = numero;
+            this.salarioEmpleado = salario;
+            this.estadoEmpleado = estado;
+        }
+
+
+        //Metodo para mostrar los datos del empleado
+        public void MostrarDatos()
+        {
+            string estado = (estadoEmpleado) ? "Activo" : "Inactivo";
+
+            Console.WriteLine($"Nombre: {nombreEmpleado}, Numero De Empleado: {numeroEmpleado}, Salario: {salarioEmpleado}, Estado: {estado}");
+        }
+
+        //Metodo para obtener el numero del empleado
+        public int ObtenerNumeroEmpleado()
+        {
+            return this.numeroEmpleado;
+        }
+
+        //Metodo para cambiar el estado del empleado
+        public void CambiarEstadoEmpleado(int nuevoEstado)
+        {
+
+
+            Console.WriteLine("...............................................");
+            Console.WriteLine("Cambio de estado de el numero de empleado: " + this.numeroEmpleado);
+            Console.WriteLine("...............................................");
+            Console.WriteLine();
+
+            if (nuevoEstado == 1)
+            {
+
+                estadoEmpleado = true;
+
+                Console.WriteLine("Empleado activo");
+                Console.WriteLine();
+
+            }
+            else if (nuevoEstado == 0)
+            {
+
+                estadoEmpleado = false;
+
+                Console.WriteLine("Empleado inactivo.");
+                Console.WriteLine();
+
+            }
+            else
+            {
+
+                Console.WriteLine("Estado no valido.");
+
+            }
+
+        }
+
+    }
+}
---
+++ EjercicioDeEmpleado/EjercicioDeEmpleado/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeEmpleado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Aplicacion de Gestion de Empleados");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine();
+
+            //Creamos una lista para guardar datos de los empleados
+
+            List<Empleado> listaEmpleados = new List<Empleado>();
+
+            //Agregamos datos a la lista
+
+            listaEmpleados.Add(new Empleado("Juan", 101, 30000));
+            listaEmpleados.Add(new Empleado("Maria", 102, 35000));
+            listaEmpleados.Add(new Empleado("Pedro", 103, 32000, false));
+            Console.WriteLine();
+
+            //Mostramos los datos de cada empleado
+
+            foreach (var empleado in listaEmpleados)
+            {
+                empleado.MostrarDatos();
+
+            }
+            Console.WriteLine();
+
+
+            //Cambiamos el estado de un empleado por su numero de empleado
+
+            int numeroEmpleadoCambiarEstado = 103;
+
+            Empleado empleadoCambiarEstado = BuscarEmpleado(listaEmpleados, numeroEmpleadoCambiarEstado);
+
+            if (empleadoCambiarEstado != null)
+            {
+
+                empleadoCambiarEstado.CambiarEstadoEmpleado(1);
+                Console.WriteLine();
+
+                empleadoCambiarEstado.MostrarDatos();
+    
+
+            }
+            else
+            {
+
+
+                Console.WriteLine($"Empleado con numero {numeroEmpleadoCambiarEstado} no encontrado.");
+
+            }
+
+        }
+
+        //Metodo para buscar un empleado por su numero
+        static Empleado BuscarEmpleado(List<Empleado> empleados, int numeroEmpleado)
+        {
+
+            foreach (var empleado in empleados)
+            {
+
+                if (empleado.ObtenerNumeroEmpleado() == numeroEmpleado)
+                {
+
+                    return empleado;
+
+                }
+                
+            }
+
+            return null;
+
+        }
+    }
+}
